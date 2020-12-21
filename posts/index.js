6d5caf385b0d08f9e3d1ea3e9b293a066d920f99
Ryan <Ryangarofalo100@gmail.com>
2020-12-21 10:17:23 -0500

    Checkpoint 76

--- posts/index.js
+++ posts/index.js
@@ -26,7 +26,7 @@ app.post('/posts', async (req, res) => {
     title
   };
 
-  await axios.post('http://localhost:4005/events',{
+  await axios.post('http://event-bus-srv:4005/events',{
     type: 'PostCreated',
     data: {
       id, title
@@ -44,7 +44,7 @@ app.post('/events', (req, res) => {
 });
 
 app.listen(4000, () => {
-console.log('v20');
+console.log('v55');
 console.log('Listening on 4000');
 
 });
